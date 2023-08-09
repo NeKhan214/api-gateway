087ca2431a8c63f564c351384220383d2acc7814
NehaK214 <er.nehakhan@gmail.com>
2023-08-09 09:27:26 +0530

    Api-Gateway add code logic for routing request to for employee and department microservice

---
+++ src/test/java/com/nehak/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nehak.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
